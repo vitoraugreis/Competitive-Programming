2213275b77ed45b883e7cd22eb02a274603f127a
vitoraugreis <vitoraugreis@gmail.com>
2025-07-14 19:47:47 -0300

    LeetCode 1290

--- LeetCode/1290.cpp
+++ LeetCode/1290.cpp
@@ -5,6 +5,14 @@
 ---------------------------------------------------------------
 */
 
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+
 class Solution {
 public:
     int getDecimalValue(ListNode* head) {
@@ -16,15 +24,3 @@ public:
         return decimal;
     }
 };
-
-
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
