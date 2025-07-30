2485f0a5daeacb1a53406e9f73b3eae1e324df07
vitoraugreis <vitoraugreis@gmail.com>
2025-07-30 14:24:07 -0300

    LeetCode 11

---
+++ LeetCode/11.cpp
@@ -0,0 +1,31 @@
+/*
+---------------------------------------------------------------
+    11. Container With Most Water
+---------------------------------------------------------------
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int ans = 0;
+        int l = 0;
+        int r = height.size()-1;
+        while (l <= r) {
+            int area = min(height[l], height[r]) * (r-l);
+            ans = (area > ans) ? area : ans;
+            if (height[l] <= height[r]) l++;
+            else r--;
+        }
+        return ans;
+    }
+};
+
+/*
+3 5 4 6 1
+
+nums(size)-1-i;
+
+*/
