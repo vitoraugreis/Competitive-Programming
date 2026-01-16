60377b470f59511ae7e8180e30776037f778c185
vitoraugreis <vitoraugreis@gmail.com>
2026-01-16 14:26:13 -0300

    LeetCode 175

---
+++ LeetCode/175.sql
@@ -0,0 +1,9 @@
+/*
+---------------------------------------------------------------
+    175. Combine Two Tables
+---------------------------------------------------------------
+*/
+
+select firstName, lastName,city, state
+from Person
+    left join Address on Person.personID = Address.personId
