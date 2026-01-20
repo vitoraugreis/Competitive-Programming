c351bb49e674b4df885bb18d6447c6c641f64433
vitoraugreis <vitoraugreis@gmail.com>
2026-01-20 14:30:06 -0300

    LeetCode 1148

---
+++ LeetCode/1148.sql
@@ -0,0 +1,10 @@
+/*
+---------------------------------------------------------------
+    1148. Article Views I
+---------------------------------------------------------------
+*/
+
+SELECT DISTINCT author_id AS id
+FROM Views
+WHERE author_id = viewer_id
+ORDER BY author_id ASC
